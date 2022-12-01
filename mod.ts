d71d2dc715de8d41a9ace10af394c00b79a331d0
Ruthenic <mdrakea3@tutanota.com>
2022-12-01 00:29:44 -0500

    prepare for deno.land release(?)

---
+++ mod.ts
@@ -0,0 +1,7 @@
+import AO3 from "./src/classes/AO3.ts";
+export default AO3;
+
+export * from "./src/classes/AO3.ts";
+export * from "./src/classes/Chapter.ts";
+export * from "./src/classes/Search.ts";
+export * from "./src/classes/Work.ts";
